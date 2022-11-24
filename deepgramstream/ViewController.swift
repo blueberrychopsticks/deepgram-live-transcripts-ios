67b2e120c81a59b88e077a16eade6630b864b1ee
🫐🥢 <blueberrychopsticks@pm.me>
2022-11-24 11:33:23 -0500

    🐛 starscream: not an upgrade - fixed
    https://github.com/deepgram-devs/deepgram-live-transcripts-ios/issues/1#issuecomment-1302777064

--- deepgramstream/ViewController.swift
+++ deepgramstream/ViewController.swift
@@ -5,120 +5,145 @@
 //  Created by Abdulhakim Ajetunmobi on 30/10/2021.
 //
 
-import UIKit
 import AVFoundation
 import Starscream
+import UIKit
 
 class ViewController: UIViewController {
-    
-    private let apiKey = "Token YOUR_DEEPGRAM_API_KEY"
-    private let audioEngine = AVAudioEngine()
-    
-    private lazy var socket: WebSocket = {
-        let url = URL(string: "wss://api.deepgram.com/v1/listen?encoding=linear16&sample_rate=48000&channels=1")!
-        var urlRequest = URLRequest(url: url)
-        urlRequest.setValue(apiKey, forHTTPHeaderField: "Authorization")
-        return WebSocket(request: urlRequest)
-    }()
-    
-    private let jsonDecoder: JSONDecoder = {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        return decoder
-    }()
-    
-    private let transcriptView: UITextView = {
-        let textView = UITextView()
-        textView.isScrollEnabled = true
-        textView.backgroundColor = .lightGray
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        return textView
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        socket.delegate = self
-        socket.connect()
-        setupView()
-        startAnalyzingAudio()
+  private let apiKey = "a97a974979ad45acdddd29dfcaad9ccfb8f27dcf"
+  private var token = ""
+  private let audioEngine = AVAudioEngine()
+  var webSocketConnected: Bool = false
+
+  private lazy var socket: WebSocket = {
+    token = "Token \(apiKey)"
+    let url =
+      URL(
+        string: "wss://api.deepgram.com/v1/listen?encoding=linear16&sample_rate=48000&channels=1"
+      )!
+    var urlRequest = URLRequest(url: url)
+    urlRequest.setValue(token, forHTTPHeaderField: "Authorization")
+    return WebSocket(
+      request: urlRequest,
+      certPinner: FoundationSecurity(allowSelfSigned: true)
+    )
+  }()
+
+  private let jsonDecoder: JSONDecoder = {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    return decoder
+  }()
+
+  private let transcriptView: UITextView = {
+    let textView = UITextView()
+    textView.isScrollEnabled = true
+    textView.backgroundColor = .lightGray
+    textView.translatesAutoresizingMaskIntoConstraints = false
+    return textView
+  }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    socket.delegate = self
+    socket.connect()
+    setupView()
+    startAnalyzingAudio()
+  }
+
+  private func setupView() {
+    view.addSubview(transcriptView)
+    NSLayoutConstraint.activate([
+      transcriptView.topAnchor
+        .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      transcriptView.leadingAnchor.constraint(
+        equalTo: view.leadingAnchor,
+        constant: 16
+      ),
+      transcriptView.trailingAnchor.constraint(
+        equalTo: view.trailingAnchor,
+        constant: -16
+      ),
+      transcriptView.bottomAnchor
+        .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+    ])
+  }
+
+  private func startAnalyzingAudio() {
+    let inputNode = audioEngine.inputNode
+    let inputFormat = inputNode.inputFormat(forBus: 0)
+    let outputFormat = AVAudioFormat(
+      commonFormat: .pcmFormatInt16,
+      sampleRate: inputFormat.sampleRate,
+      channels: inputFormat.channelCount,
+      interleaved: true
+    )
+    let converterNode = AVAudioMixerNode()
+    let sinkNode = AVAudioMixerNode()
+
+    audioEngine.attach(converterNode)
+    audioEngine.attach(sinkNode)
+
+    converterNode.installTap(
+      onBus: 0,
+      bufferSize: 1024,
+      format: converterNode.outputFormat(forBus: 0)
+    ) { (buffer: AVAudioPCMBuffer!, _: AVAudioTime!) in
+      if let data = self.toNSData(buffer: buffer) {
+        self.socket.write(data: data)
+      }
     }
-    
-    private func setupView() {
-        view.addSubview(transcriptView)
-        NSLayoutConstraint.activate([
-            transcriptView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            transcriptView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            transcriptView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            transcriptView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
+
+    audioEngine.connect(inputNode, to: converterNode, format: inputFormat)
+    audioEngine.connect(converterNode, to: sinkNode, format: outputFormat)
+    audioEngine.prepare()
+
+    do {
+      try AVAudioSession.sharedInstance().setCategory(.record)
+      try audioEngine.start()
+    } catch {
+      print(error)
     }
-    
-    private func startAnalyzingAudio() {
-        let inputNode = audioEngine.inputNode
-        let inputFormat = inputNode.inputFormat(forBus: 0)
-        let outputFormat = AVAudioFormat(commonFormat: .pcmFormatInt16, sampleRate: inputFormat.sampleRate, channels: inputFormat.channelCount, interleaved: true)
-        let converterNode = AVAudioMixerNode()
-        let sinkNode = AVAudioMixerNode()
-        
-        audioEngine.attach(converterNode)
-        audioEngine.attach(sinkNode)
-        
-        converterNode.installTap(onBus: 0, bufferSize: 1024, format: converterNode.outputFormat(forBus: 0)) { (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
-            if let data = self.toNSData(buffer: buffer) {
-                self.socket.write(data: data)
-            }
-        }
-        
-        audioEngine.connect(inputNode, to: converterNode, format: inputFormat)
-        audioEngine.connect(converterNode, to: sinkNode, format: outputFormat)
-        audioEngine.prepare()
-        
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.record)
-            try audioEngine.start()
-        } catch {
-            print(error)
-        }
-    }
-    
-    private func toNSData(buffer: AVAudioPCMBuffer) -> Data? {
-        let audioBuffer = buffer.audioBufferList.pointee.mBuffers
-        return Data(bytes: audioBuffer.mData!, count: Int(audioBuffer.mDataByteSize))
-    }
+  }
+
+  private func toNSData(buffer: AVAudioPCMBuffer) -> Data? {
+    let audioBuffer = buffer.audioBufferList.pointee.mBuffers
+    return Data(bytes: audioBuffer.mData!, count: Int(audioBuffer.mDataByteSize))
+  }
 }
 
 extension ViewController: WebSocketDelegate {
-    func didReceive(event: WebSocketEvent, client: WebSocket) {
-        switch event {
-        case .text(let text):
-            let jsonData = Data(text.utf8)
-            let response = try! jsonDecoder.decode(DeepgramResponse.self, from: jsonData)
-            let transcript = response.channel.alternatives.first!.transcript
-        
-            if response.isFinal && !transcript.isEmpty {
-                if transcriptView.text.isEmpty {
-                    transcriptView.text = transcript
-                } else {
-                    transcriptView.text = transcriptView.text + " " + transcript
-                }
-            }
-        case .error(let error):
-            print(error ?? "")
-        default:
-            break
+  func didReceive(event: WebSocketEvent, client _: WebSocket) {
+    switch event {
+    case let .text(text):
+      let jsonData = Data(text.utf8)
+      let response = try! jsonDecoder.decode(DeepgramResponse.self, from: jsonData)
+      let transcript = response.channel.alternatives.first!.transcript
+
+      if response.isFinal, !transcript.isEmpty {
+        if transcriptView.text.isEmpty {
+          transcriptView.text = transcript
+        } else {
+          transcriptView.text = transcriptView.text + " " + transcript
         }
+      }
+    case let .error(error):
+      print(error ?? "")
+    default:
+      break
     }
+  }
 }
 
 struct DeepgramResponse: Codable {
-    let isFinal: Bool
-    let channel: Channel
-    
-    struct Channel: Codable {
-        let alternatives: [Alternatives]
-    }
-    
-    struct Alternatives: Codable {
-        let transcript: String
-    }
+  let isFinal: Bool
+  let channel: Channel
+
+  struct Channel: Codable {
+    let alternatives: [Alternatives]
+  }
+
+  struct Alternatives: Codable {
+    let transcript: String
+  }
 }
